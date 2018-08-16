5b1481b2c336b84f5649e5f58721696d47e4dfa9
suminjie <suminjie@onemt.com.cn>
2018-08-16 14:30:19 +0800

    swiftMonkey 测试

--- FXNewsDemo/FXNewsDemo/Module/Main/FXMainVCL.m
+++ FXNewsDemo/FXNewsDemo/Module/Main/FXMainVCL.m
@@ -17,7 +17,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+//    http://www.mobibounty.com/upload/uploadBase64
+//    base64Data
+//    fileType
     
+
+    
+//    NSURLSessionUploadTask *uploadTask = [session uploadTaskWithRequest:request fromData:<#(nullable NSData *)#> completionHandler:<#^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error)completionHandler#>]
 }
 
 - (void)didReceiveMemoryWarning {
@@ -28,6 +34,41 @@
     NSMutableDictionary *dict = [NSMutableDictionary dictionaryWithCapacity:1];
 //    [dict setObject:nil forKey:@"1"];
     NSLog(@"button1");
+    
+    NSURL *url = [NSURL URLWithString:@"http://www.mobibounty.com/upload/uploadBase64"];
+    
+    UIImage *image = [UIImage imageNamed:@"750x1334_01.jpg"];
+    NSData *data = UIImageJPEGRepresentation(image, 0.8f);
+    NSString *base64ImageStr = [data base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+    base64ImageStr = [base64ImageStr stringByReplacingOccurrencesOfString:@"+"withString:@"%2B"];
+    NSURLSession *session = [NSURLSession sharedSession];
+    
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:url];
+    request.HTTPMethod = @"POST";
+    NSString *body = [NSString stringWithFormat:@"base64Data=%@&fileType=jpg",base64ImageStr];
+    request.HTTPBody = [body dataUsingEncoding:NSUTF8StringEncoding];
+
+    NSURLSessionTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+
+        NSString *responseString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        NSLog(@"$=%@, resStr = %@",response,responseString);
+    }];
+    [request setValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    
+//    NSDictionary *parameter = @{@"base64Data":base64ImageStr,
+//                                @"fileType":@"jpg"
+//                                };
+//    request.HTTPBody = [NSJSONSerialization dataWithJSONObject:parameter
+//                                                       options:NSJSONWritingPrettyPrinted
+//                                                         error:nil];
+//
+//    NSURLSessionTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+//
+//        NSString *responseString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+//        NSLog(@"$=%@, resStr = %@",response,responseString);
+//    }];
+    [task resume];
+    
 }
 - (IBAction)button2Clicked:(id)sender {
     NSLog(@"button2");
